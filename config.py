27496bdcfec06bd9361d5c6ed2c49f7e07987bb7
sadegh <msh.habibian@gmail.com>
2026-03-27 12:49:02 +0330

    add token

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 # config.py
 
 # Replace with your actual bot token
-BOT_TOKEN = "1421133829:AgGZYsLFZIh3BDcJ6zoY4TWRRa5eYorP_9c"
+# hyvana bot
+# BOT_TOKEN = "1421133829:AgGZYsLFZIh3BDcJ6zoY4TWRRa5eYorP_9c"
+# msh bot
+BOT_TOKEN = "1855140195:kYt1ItznwXwZ2kSa9BTLPkgK9QoDUdVUld4"
 
 # Maximum file size allowed by Telegram Bot API (50 MB)
 MAX_FILE_SIZE = 50 * 1024 * 1024
