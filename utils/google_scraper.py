6ffff25bd0b47fb36e33a7a39312fa3045549317
sadegh <msh.habibian@gmail.com>
2026-03-29 17:03:47 +0330

    add logs

--- utils/google_scraper.py
+++ utils/google_scraper.py
@@ -8,13 +8,14 @@ def search_google(query, num_results=10):
     Keeps the function name 'search_google' so you don't have to change your imports.
     """
     results = []
-
+    print("\n\n input query: ", query)
     try:
         with DDGS() as ddgs:
             # max_results limits the number of fetched items
             search_results = ddgs.text(query, max_results=num_results)
-
+            print("search res:", search_results)
             for res in search_results:
+                print("res:", res)
                 results.append(
                     {
                         "title": res.get("title", "بدون عنوان"),  # Default fallback
