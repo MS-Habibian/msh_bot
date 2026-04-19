40a6eb3326ae1a9e7b8f72671d83e2f02700b3f2
sharifi <mehdifcb7997@gmail.com>
2026-04-20 00:24:10 +0330

    add paper first phase

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from handlers.downloader import download_command, handle_reupload_callback
 from handlers.google import google_command
 from handlers.image import image_command
 # from handlers.instagram import instagram_command
-from utils.scholar_utils import scholar_command
 from handlers.youtube import handle_yt_format_callback, yt_command, handle_yt_download_callback, ytdl_command
 from handlers.pinterest import pin_command, pin_download_callback
 from handlers.tgposts import handle_download_rar_button, handle_reupload_tg_button, tgposts_command
