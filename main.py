fa88e210ec2b061fc090b805575f8a50f0beec8b
sharifi <mehdifcb7997@gmail.com>
2026-04-24 21:38:19 +0330

    first phase for podcast

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from handlers.youtube import handle_yt_format_callback, yt_command, handle_yt_do
 from handlers.pinterest import pin_command, pin_download_callback
 from handlers.tgposts import handle_download_rar_button, handle_reupload_tg_button, tgposts_command
 from handlers.commands import start_command, help_command, help_callback_handler
-from handlers.podcast import handle_pod_callback, pod_command
+from handlers.podcast import handle_pod_callback, pod_command, podcast_command
 from telegram.ext import CommandHandler, CallbackQueryHandler
 
 # Import Scholar Handlers
@@ -85,7 +85,7 @@ def main() -> None:
     application.add_handler(CallbackQueryHandler(paper_download_callback, pattern=r"^paper_pdf\|"))
     application.add_handler(CallbackQueryHandler(paper_paginate_callback, pattern=r"^scholar_page\|"))
     # در بخشی که هندلرهای یوتیوب را Add می‌کنید، این خطوط را اضافه کنید:
-    application.add_handler(CommandHandler("podcast", pod_command))
+    application.add_handler(CommandHandler("podcast", podcast_command))
     # application.add_handler(CallbackQueryHandler(handle_pod_download_callback, pattern='^poddl:'))
     # تغییر پترن به گونه‌ای که هر دو poddl و podmore را بگیرد
     application.add_handler(CallbackQueryHandler(handle_pod_callback, pattern='^pod(dl|more):'))
